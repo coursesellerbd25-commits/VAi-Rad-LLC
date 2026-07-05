b531fe96ac791b6d4161ac0de7e323bbd7813541
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-05 18:42:46 +0600

    Add Django backend folder

---
+++ 404-project/backend/annotations/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    filename = models.CharField(max_length=255, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            self.filename = self.image.name.split("/")[-1]
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.filename
+
+class Polygon(models.Model):
+    image = models.ForeignKey(
+        Image,
+        on_delete=models.CASCADE,
+        related_name="polygons"
+    )
+
+    points = models.JSONField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Polygon #{self.id} - {self.image.filename}"
